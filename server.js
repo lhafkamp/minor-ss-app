a035fcb363e1ce12b1bd85ed1b70c60dbac031fd
lhafkamp <Luuk@096.152.028.145.hva.nl>
2017-03-21 14:54:41 +0100

    new setup

--- server.js
+++ server.js
@@ -2,18 +2,26 @@
 const express = require('express');
 const path = require('path');
 
-// routers
-const indexRouter = require('./routes/index');
-
 // app = express
 const app = express();
+
+// get the public files
+app.use(express.static('public'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// connect routers to routes
-app.use('/', indexRouter);
+// render the index page
+app.get('/', (req, res) => {
+	res.render('index', {
+		title: 'bes title',
+	});
+});
+
+app.get('/data', (req, res) => {
+	res.render('data');
+});
 
 // run app on 9000
 app.listen(9000, () => {
