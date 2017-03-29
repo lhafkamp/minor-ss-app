7266c9af7a53417179ede93393f9d5eaed6715dd
Luuk Hafkamp <luukhafkamp@gmail.com>
2017-03-29 12:08:21 +0200

    Feature/serviceworker (#7)
    * serviceworker working
    
    * working for the index page
    
    * css routing problem fixed

---
+++ public/js/app.js
@@ -0,0 +1,9 @@
+// if serviceworker is supported
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker.register('./sw.js', { scope: './' }).then((registration) => {
+		console.log('SW found');
+	}).catch((err) => {
+		console.log('SW not found', err);
+	});
+}
+
